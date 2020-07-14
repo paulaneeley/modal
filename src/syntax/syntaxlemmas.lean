953e242cf8d2caa084e3fbcdd8d0401825852d6f
Paula Neeley <paulaneeley@gmail.com>
2020-07-14 15:25:39 -0700

    Update syntaxlemmas.lean

--- src/syntax/syntaxlemmas.lean
+++ src/syntax/syntaxlemmas.lean
@@ -1,7 +1,7 @@
 -- Following the textbook "Dynamic Epistemic Logic" by 
 -- Hans van Ditmarsch, Wiebe van der Hoek, and Barteld Kooi
 
-import language syntax.syntax data.set.basic
+import language syntax.syntax data.set.basic syntax.soundness
 
 open prfK
 
@@ -11,28 +11,8 @@ open prfK
 
 lemma iden {Γ : ctx} {φ : form} :
   prfK Γ (φ ⊃ φ) :=
-  mp (mp (@pl2 _ φ (φ ⊃ φ) φ) pl1) pl1
-
-
-/- structural lemmas -/
-lemma sub_weak {Γ Δ : ctx} {φ : form} :
-  (prfK Δ φ) → (Δ ⊆ Γ) → (prfK Γ φ) :=
 begin
-  intros h s,
-  induction h,
-  { apply ax, exact s h_h },
-  { exact pl1 },
-  { exact pl2 },
-  --{ exact pl3 },
-  { exact pl4 },
-  { exact pl5 },
-  { exact pl6 },
-  { exact pl7 },
-  { exact kdist },
-  { apply mp,
-    { exact h_ih_hpq s },
-    {exact h_ih_hp s} },
-  { exact nec (h_ih s) }
+exact mp (mp (@pl2 _ φ (φ ⊃ φ) φ) pl1) pl1
 end
 
 
@@ -55,6 +35,193 @@ begin
     { exact h_ih_hpq },
     { exact h_ih_hp } },
   { exact nec h_ih }
+end
+
+
+lemma pr {Γ : ctx} {φ : form} :
+  prfK (Γ ∪ φ) φ :=
+begin
+apply ax;
+apply or.intro_left;
+simp
+end
+
+
+lemma cut {Γ : ctx} {φ ψ χ : form} :
+  prfK Γ (φ ⊃ ψ) → prfK Γ (ψ ⊃ χ) → prfK Γ (φ ⊃ χ) :=
+begin
+intros h1 h2,
+exact mp (mp pl2 (mp pl1 h2)) h1
+end
+
+
+lemma conv_deduction {Γ : ctx} {φ ψ : form} :
+  prfK Γ (φ ⊃ ψ) → prfK (Γ ∪ φ) ψ :=
+begin
+intro h, 
+exact mp (weak h) pr 
+end
+
+
+lemma imp_if_imp_imp {Γ : ctx} {φ ψ χ : form} : prfK Γ (φ ⊃ χ) → prfK Γ (φ ⊃ (ψ ⊃ χ)) :=
+begin
+intro h1,
+exact mp (mp pl2 (mp pl1 pl1)) h1
+end
+
+
+lemma cut1 {Γ : ctx} {φ ψ χ θ : form} :
+  prfK Γ (θ ⊃ (φ ⊃ ψ)) → prfK Γ (ψ ⊃ χ) → prfK Γ (θ ⊃ (φ ⊃ χ)) :=
+begin
+intros h1 h2,
+have h3 : prfK Γ (φ ⊃ ψ) → prfK Γ (ψ ⊃ χ) → prfK Γ (φ ⊃ χ), from cut,
+have h4 : prfK Γ (θ ⊃ (φ ⊃ ψ)) → prfK Γ ((φ ⊃ ψ) ⊃ (φ ⊃ χ)) → prfK Γ (θ ⊃ (φ ⊃ χ)), from cut,
+specialize h4 h1,
+have h5 : prfK Γ ((φ ⊃ ψ) ⊃ (φ ⊃ χ)), from mp pl2 (mp pl1 h2),
+specialize h4 h5,
+exact h4
+end
+
+
+lemma imp_switch {Γ : ctx} {φ ψ χ : form} : prfK Γ (φ ⊃ (ψ ⊃ χ)) → prfK Γ (ψ ⊃ (φ ⊃ χ)) :=
+begin
+intro h1,
+have h2 : prfK Γ (ψ ⊃ ((φ ⊃ ψ) ⊃ (φ ⊃ χ))), from mp pl1 (mp pl2 h1),
+have h3 : prfK Γ ((ψ ⊃ (φ ⊃ ψ)) ⊃ (ψ ⊃ (φ ⊃ χ))), from mp pl2 h2,
+exact mp h3 pl1
+end
+
+
+lemma cut2 {Γ : ctx} {φ ψ χ θ : form} :
+  prfK Γ (φ ⊃ ψ) → prfK Γ (θ ⊃ (ψ ⊃ χ)) → prfK Γ (θ ⊃ (φ ⊃ χ)) :=
+begin
+intros h1 h2,
+exact imp_switch (cut h1 (imp_switch h2))
+end
+
+
+lemma double_imp {Γ : ctx} {φ ψ : form} :
+  prfK Γ ((φ ⊃ (φ ⊃ ψ)) ⊃ (φ ⊃ ψ)) :=
+begin
+have h1 : prfK Γ ((φ ⊃ ((φ ⊃ ψ) ⊃ ψ)) ⊃ ((φ ⊃ (φ ⊃ ψ)) ⊃ (φ ⊃ ψ))), from pl2,
+have h2 : prfK Γ ((φ ⊃ ψ) ⊃ (φ ⊃ ψ)), from iden,
+have h3 : prfK Γ ((φ ⊃ ψ) ⊃ (φ ⊃ ψ)) → prfK Γ (φ ⊃ ((φ ⊃ ψ) ⊃ ψ)), from imp_switch,
+specialize h3 h2,
+exact mp h1 h3
+end
+
+
+lemma imp_imp_iff_imp {Γ : ctx} {θ φ ψ : form} : 
+  prfK Γ (θ ⊃ (φ ⊃ (φ ⊃ ψ))) ↔ prfK Γ (θ ⊃ (φ ⊃ ψ)) :=
+begin
+split,
+{intro h1,
+exact cut h1 double_imp},
+{intro h1,
+exact cut h1 pl1}
+end
+
+
+lemma imp_shift {Γ : ctx} {θ φ ψ χ : form} : 
+  prfK Γ (θ ⊃ (φ ⊃ (ψ ⊃ χ))) ↔ prfK Γ (θ ⊃ (ψ ⊃ (φ ⊃ χ))) :=
+begin
+split,
+repeat {intro h1, exact cut h1 (cut2 pl1 pl2)}
+end
+
+
+lemma left_and_imp {Γ : ctx} {φ ψ χ : form} :
+  prfK Γ (ψ ⊃ ((φ & ψ) ⊃ χ)) → prfK Γ ((φ & ψ) ⊃ χ) :=
+begin
+intro h1,
+exact mp double_imp (cut pl6 h1)
+end
+
+
+lemma and_right_imp {Γ : ctx} {φ ψ χ : form} : 
+  prfK Γ ((φ & ψ) ⊃ χ) ↔ prfK Γ (ψ ⊃ (φ ⊃ χ)) :=
+begin
+split, 
+{intro h1,
+exact mp (cut2 pl1 pl2) (cut1 pl4 h1)},
+intro h1,
+exact left_and_imp (cut2 pl5 h1)
+end
+
+
+--TODOs:
+lemma not_contra1 {Γ : ctx} {φ : form} : 
+  ¬ prfK Γ (φ & ¬φ) :=
+begin
+simp, by_contradiction h1,
+have h2 : prfK Γ ((φ & ¬φ) ⊃ φ), from pl5,
+have h3 : prfK Γ ((φ & ¬φ) ⊃ ¬φ), from pl6,
+have h4 : prfK (Γ ∪ (φ & ¬φ)) φ, from conv_deduction h2,
+have h5 : prfK (Γ ∪ (φ & ¬φ)) ¬φ, from conv_deduction h3,
+have h6 : sem_csq (Γ ∪ (φ & ¬φ)) φ, from soundness (Γ ∪ (φ & ¬φ)) φ h4,
+have h7 : sem_csq (Γ ∪ (φ & ¬φ)) ¬φ, from soundness (Γ ∪ (φ & ¬φ)) (¬φ) h5,
+rw sem_csq at *, dsimp at h6, dsimp at h7, sorry
+end
+
+
+lemma not_contra {Γ : ctx} {φ : form} : 
+  prfK Γ ¬(φ & ¬φ) :=
+begin
+simp, sorry
+end
+
+
+
+
+
+
+
+
+
+
+
+-- DEPRECATED:
+/-
+-- structural lemmas
+lemma sub_weak {Γ Δ : ctx} {φ : form} :
+  (prfK Δ φ) → (Δ ⊆ Γ) → (prfK Γ φ) :=
+begin
+  intros h s,
+  induction h,
+  { apply ax, exact s h_h },
+  { exact pl1 },
+  { exact pl2 },
+  --{ exact pl3 },
+  { exact pl4 },
+  { exact pl5 },
+  { exact pl6 },
+  { exact pl7 },
+  { exact kdist },
+  { apply mp,
+    { exact h_ih_hpq s },
+    {exact h_ih_hp s} },
+  { exact nec (h_ih s) }
+end
+
+
+lemma sub_weak {Γ Δ : ctx} {φ : form} :
+  (prfK Δ φ) → (Δ ⊆ Γ) → (prfK Γ φ) :=
+begin
+  intros h s,
+  induction h,
+  { apply ax, exact s h_h },
+  { exact pl1 },
+  { exact pl2 },
+  --{ exact pl3 },
+  { exact pl4 },
+  { exact pl5 },
+  { exact pl6 },
+  { exact pl7 },
+  { exact kdist },
+  { apply mp,
+    { exact h_ih_hpq s },
+    {exact h_ih_hp s} },
+  { exact nec (h_ih s) }
 end
 
 
@@ -115,7 +282,7 @@ lemma exg {Γ : ctx} {φ ψ χ : form} :
 end
 
 
-/- subcontext lemmas -/
+-- subcontext lemmas
 lemma subctx_ax {Γ Δ : ctx} {φ : form} :
    Δ ⊆ Γ → prfK Δ φ → prfK Γ φ :=
 begin
@@ -161,46 +328,27 @@ begin
 end
 
 
-/- right-hand side rules of inference -/
-lemma pr {Γ : ctx} {φ : form} :
-  prfK (Γ ∪ φ) φ :=
-by apply ax; apply or.intro_left; simp
-
 lemma pr1 {Γ : ctx} {φ ψ : form} :
   prfK (Γ ∪ φ ∪ ψ) φ :=
 by apply ax; apply or.intro_right; apply or.intro_left; simp
+
 
 lemma pr2 {Γ : ctx} {φ ψ : form} :
   prfK (Γ ∪ φ ∪ ψ) ψ :=
 by apply ax; apply or.intro_left; simp
 
+
 lemma by_mp1 {Γ : ctx} {φ ψ : form} :
   prfK (Γ ∪ φ ∪ (φ ⊃ ψ)) ψ :=
 mp pr2 pr1
+
 
 lemma by_mp2 {Γ : ctx} {φ ψ : form} :
   prfK (Γ ∪ (φ ⊃ ψ) ∪ φ) ψ :=
 mp pr1 pr2
 
-lemma cut {Γ : ctx} {φ ψ χ : form} :
-  prfK Γ (φ ⊃ ψ) → prfK Γ (ψ ⊃ χ) → prfK Γ (φ ⊃ χ) :=
-λ hpq hqr, mp (mp pl2 (mp pl1 hqr)) hpq
 
-lemma conv_deduction {Γ : ctx} {φ ψ : form} :
-  prfK Γ (φ ⊃ ψ) → prfK (Γ ∪ φ) ψ :=
-λ h, mp (weak h) pr 
-
-lemma not_pp {Γ : ctx} {φ : form} :
-  prfK Γ ((¬φ) ⊃ (φ ⊃ φ)) :=
-  begin
-  have h1 : prfK Γ ((φ ⊃ φ) ⊃ ((¬φ) ⊃ (φ ⊃ φ))), from pl1,
-  have h2 : prfK Γ (φ ⊃ φ), from iden,
-  have h3 : prfK Γ ((¬φ) ⊃ (φ ⊃ φ)), from mp h1 h2,
-  exact h3
-  end
-
-
-/- left-hand side rules of inference -/
+-- left-hand side rules of inference
 lemma mp_in_ctx_left {Γ : ctx} {φ ψ χ : form} :
   prfK (Γ ∪ φ ∪ ψ) χ → prfK (Γ ∪ φ ∪ (φ ⊃ ψ)) χ :=
 begin
@@ -260,54 +408,17 @@ begin
 end
 
 
-lemma dni {Γ : ctx} {φ : form} :
-  prfK Γ (φ ⊃ (¬¬φ)) := sorry
-
-
---TODOs:
-lemma not_contra {Γ : ctx} {φ : form} : 
-  prfK Γ ¬(φ & ¬φ) :=
+lemma iden_imp_imp {Γ : ctx} {φ ψ : form} : prfK Γ (((φ ⊃ φ) ⊃ (φ ⊃ ψ)) ⊃ (φ ⊃ ψ)) :=
 begin
-simp, 
-have h1 : prfK Γ ((φ & ¬φ) ⊃ φ), from pl5,
-have h2: prfK Γ ((φ & ¬φ) ⊃ ¬φ), from pl6,
-sorry
+exact cut pl2 weak_imp
 end
 
 
-lemma and_left_imp {Γ : ctx} {φ ψ χ : form} : 
-  prfK Γ ((φ & ψ) ⊃ χ) ↔ prfK Γ (φ ⊃ (ψ ⊃ χ)) :=
+lemma not_id {Γ : ctx} {φ : form} :
+  prfK Γ ((¬φ) ⊃ (φ ⊃ φ)) :=
 begin
-split,
-{intro h, sorry}, 
-sorry
+exact mp pl1 iden
 end
 
 
-lemma and_right_imp {Γ : ctx} {φ ψ χ : form} : 
-  prfK Γ ((φ & ψ) ⊃ χ) ↔ prfK Γ (ψ ⊃ (φ ⊃ χ)) :=
-begin
-split, intro h1, sorry, sorry
-end
-
-
-lemma imp_if_imp_imp {Γ : ctx} {φ ψ χ : form} : prfK Γ (φ ⊃ χ) → prfK Γ (φ ⊃ (ψ ⊃ χ)) :=
-begin
-intro h1,
-have h2 : prfK Γ ((φ ⊃ (ψ ⊃ χ)) ⊃ ((φ ⊃ ψ) ⊃ (φ ⊃ χ))), from pl2,
-sorry
-end
-
-
-lemma imp_imp_if_imp {Γ : ctx} {θ φ ψ : form} : 
- prfK Γ (θ ⊃ (φ ⊃ (φ ⊃ ψ))) ↔ prfK Γ (θ ⊃ (φ ⊃ ψ)) :=
-begin
-sorry
-end
-
-
-lemma imp_shift {Γ : ctx} {θ φ ψ χ : form} : 
- prfK Γ (θ ⊃ (φ ⊃ (ψ ⊃ χ))) ↔ prfK Γ (θ ⊃ (ψ ⊃ (φ ⊃ χ))) :=
-begin
-sorry
-end
+-/
